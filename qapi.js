bc63069e13370118fe697aad55f0f0e5057b346f
aesarine-ru <69194155+aesarine-ru@users.noreply.github.com>
2020-09-09 12:47:10 +0300

    Some changes

---
+++ qapi.js
@@ -0,0 +1,24 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5f58a474a201941d28e1a26d",
+				"5f58a474a201941d28e1a26f"
+			]
+		},
+		"5f58a474a201941d28e1a26d": {
+			"id": "5f58a474a201941d28e1a26d",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5f58a474a201941d28e1a26f": {
+			"id": "5f58a474a201941d28e1a26f",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"projectType": "gatsby"
+}
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Button } from "@quarkly/widgets";
+export default (() => {
+	return <Theme theme={theme}>
+		<Button>
+			Button
+		</Button>
+	</Theme>;
+});
