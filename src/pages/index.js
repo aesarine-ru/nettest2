c16cdc98f927ae0c6d487934fc8d1d97e88ba964
aesarine-ru <69194155+aesarine-ru@users.noreply.github.com>
2020-09-09 12:54:17 +0300

    Some changes 12345678

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import theme from "theme";
-import { Theme, Button } from "@quarkly/widgets";
+import { Theme, Button, Icon, Input } from "@quarkly/widgets";
+import { MdFace } from "react-icons/md";
 export default (() => {
 	return <Theme theme={theme}>
 		<Button>
 			Button
 		</Button>
+		<Icon category="md" icon={MdFace} />
+		<Input />
 	</Theme>;
 });
